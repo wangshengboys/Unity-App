c11d9be7374fa39257b765ea2b32ef214f482683
Your Name <you@example.com>
2026-04-23 22:06:28 +0700

    gas

--- PrototypingAlfa/tugas_application_1/lib/config.dart
+++ PrototypingAlfa/tugas_application_1/lib/config.dart
@@ -1,3 +1,4 @@
 class Config {
-  static const String baseUrl = "http://192.168.1.2:5000";
+static const String baseUrl = "http://10.0.2.2:5000";
+//static const String baseUrl = "http://192.168.1.2:5000";
 }
