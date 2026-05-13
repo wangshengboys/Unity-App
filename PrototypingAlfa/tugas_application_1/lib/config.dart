fb4c3cfce299da6282d8ba02016ff9633cd0435d
Centaury <bintangtimursip@gmail.com>
2026-05-13 11:15:28 +0700

    yes

--- PrototypingAlfa/tugas_application_1/lib/config.dart
+++ PrototypingAlfa/tugas_application_1/lib/config.dart
@@ -1,3 +1,8 @@
 class Config {
+<<<<<<< HEAD
   static const String baseUrl = "http://192.168.1.11:5000";
+=======
+static const String baseUrl = "http://10.0.2.2:5000";
+//static const String baseUrl = "http://192.168.1.2:5000";
+>>>>>>> c11d9be7374fa39257b765ea2b32ef214f482683
 }
