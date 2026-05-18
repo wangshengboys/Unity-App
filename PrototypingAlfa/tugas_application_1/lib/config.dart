3f625c7f850e509e8e306e302a71876ffd6f9baa
Centaury <bintangtimursip@gmail.com>
2026-05-18 17:35:03 +0700

    cloud berhasil

--- PrototypingAlfa/tugas_application_1/lib/config.dart
+++ PrototypingAlfa/tugas_application_1/lib/config.dart
@@ -1,4 +1,5 @@
 class Config {
   //static const String baseUrl = "http://10.0.2.2:5000";
-  static const String baseUrl = "http://192.168.1.7:5000";
+  //static const String baseUrl = "http://192.168.1.7:5000";
+  static const String baseUrl = "http://188.166.212.78:5000";
 }
