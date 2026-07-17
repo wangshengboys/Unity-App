46f94b07d2ae18bf9fefa1446c643523f1f23d53
Your Name <you@example.com>
2026-07-17 18:20:49 +0700

    feat: initialize main.dart with authentication session check using SharedPreferences and ProviderScope

--- PrototypingAlfa/tugas_application_1/lib/main.dart
+++ PrototypingAlfa/tugas_application_1/lib/main.dart
@@ -5,7 +5,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'screens/auth/login_page.dart';
 import 'screens/main_screen.dart';
-import 'screens/admin_dashboard.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -75,73 +74,6 @@ class MyApp extends StatelessWidget {
           //home: const DevLauncherPage(),
         );
       },
-    );
-  }
-}
-
-class DevLauncherPage extends StatelessWidget {
-  const DevLauncherPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[900],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "PROJECT LAUNCHER",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 50),
-
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 40,
-                  vertical: 20,
-                ),
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.white,
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const LoginPage()),
-                );
-              },
-              icon: const Icon(Icons.smartphone),
-              label: const Text("BUKA APLIKASI UTAMA"),
-            ),
-
-            const SizedBox(height: 20),
-
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 40,
-                  vertical: 20,
-                ),
-                backgroundColor: Colors.amber,
-                foregroundColor: Colors.black,
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const AdminDashboard()),
-                );
-              },
-              icon: const Icon(Icons.admin_panel_settings),
-              label: const Text("BUKA ADMIN DASHBOARD"),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
